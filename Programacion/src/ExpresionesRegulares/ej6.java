74321b899ebc69032e1a3d748f01ea5e968aa838
nereazj544 <nzjvirtual@gmail.com>
2024-02-22 01:23:40 +0100

    Escribe un programa que lea un documento de texto que encierre parte de su contenido entre etiquetas <uppercase> … </uppercase> y lo transforme convirtiendo a mayúsculas el contenido encerrado entre estas etiquetas y suprimiendo las mismas.

---
+++ Programacion/src/ExpresionesRegulares/ej6.java
@@ -0,0 +1,36 @@
+package ExpresionesRegulares;
+
+import java.io.File;
+import java.text.FieldPosition;
+import java.util.Scanner;
+
+public class ej6 {
+    public static void main(String[] args) {
+        try {
+            File file = new File("Programacion\\src\\ExpresionesRegulares\\ejercicio6.txt");
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String linea = sc.nextLine();
+                linea = mayus(linea);
+                System.out.println(linea);
+            }
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace(); //!Para que te salte el error bien guapo
+        }
+    }
+
+    private static String mayus(String linea) {
+        while (linea.contains("<uppercase>") && linea.contains("</uppercase>")) {
+            int inicio = linea.indexOf("<uppercase>");
+            int fin = linea.indexOf("</uppercase>");
+            String tran = linea.substring(inicio + "<uppercase>".length(), fin);
+            String t = tran.toUpperCase();
+            linea =  linea.substring(0, inicio) + t + linea.substring(fin + "</uppercase>".length());
+        }
+        return linea;
+    }
+
+    
+}
