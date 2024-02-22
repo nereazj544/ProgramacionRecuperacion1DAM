db6338aeca16fffc82822745f7c3738171a49f86
nereazj544 <nzjvirtual@gmail.com>
2024-02-22 17:25:40 +0100

    commit

--- Programacion/src/ExamenesAntiguos/segundaevaluacion/mainVehiculo.java
+++ Programacion/src/ExamenesAntiguos/segundaevaluacion/mainVehiculo.java
@@ -1,5 +1,18 @@
 package ExamenesAntiguos.segundaevaluacion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class mainVehiculo {
-    
+    public static void main(String[] args) {
+    List<Vehiculo> vehiculos = new ArrayList<>();
+        vehiculos.add(new Coche("1234 ABC", 5));
+        // Añade otros vehículos...
+
+        int dias = 7;  // Por ejemplo, alquiler de 7 días
+        for (Vehiculo vehiculo : vehiculos) {
+            double precio = vehiculo.getPrecioAlquiler(dias);
+            System.out.println("El precio de alquiler del " + vehiculo + " es " + precio + " euros.");
+        }
+    }
 }
